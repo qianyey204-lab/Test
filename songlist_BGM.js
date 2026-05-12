5b2a51fc9e06a61fc5e34f9000ab9861c6e6b004
qianyey204-lab <qianyey204@gmail.com>
2026-05-12 09:04:11 +0900

    Add files via upload

---
+++ songlist_BGM.js
@@ -0,0 +1,543 @@
+// songlist_bgm.js
+export const songListBGM = [
+  {
+    no: 1,
+    title: "「いちご白書」をもう一度",
+    artist: "バンバン",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/%E3%80%8Cいちご白書%E3%80%8Dをもう一度_バンバン.mp3"
+  },
+  {
+    no: 2,
+    title: "15の夜",
+    artist: "尾崎豊",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/15の夜_尾崎豊.mp3"
+  },
+  {
+    no: 3,
+    title: "22才の別れ",
+    artist: "風",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/22才の別れ_風.mp3"
+  },
+  {
+    no: 4,
+    title: "Automatic",
+    artist: "宇多田ヒカル",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/Automatic_宇多田ヒカル.mp3"
+  },
+  {
+    no: 5,
+    title: "BOY MEETS GIRL",
+    artist: "TRF",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/BOY%20MEETS%20GIRL_TRF.mp3"
+  },
+  {
+    no: 6,
+    title: "DEPARTURES",
+    artist: "globe",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/DEPARTURES_globe.mp3"
+  },
+  {
+    no: 7,
+    title: "EZ DO DANCE",
+    artist: "TRF",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/EZ%20DO%20DANCE_TRF.mp3"
+  },
+  {
+    no: 8,
+    title: "HERO",
+    artist: "甲斐バンド",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/HERO_甲斐バンド.mp3"
+  },
+  {
+    no: 9,
+    title: "love the island",
+    artist: "鈴木あみ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/love%20the%20island_鈴木あみ.mp3"
+  },
+  {
+    no: 10,
+    title: "M",
+    artist: "浜崎あゆみ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/M_浜崎あゆみ.mp3"
+  },
+  {
+    no: 11,
+    title: "Mr サマータイム",
+    artist: "サーカス",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/Mr%20サマータイム_サーカス.mp3"
+  },
+  {
+    no: 12,
+    title: "POP STAR",
+    artist: "平井堅",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/POP%20STAR_平井堅.mp3"
+  },
+  {
+    no: 13,
+    title: "RIDE ON TIME",
+    artist: "山下達郎",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/RIDE%20ON%20TIME_山下達郎.mp3"
+  },
+  {
+    no: 14,
+    title: "Romanticが止まらない",
+    artist: "C-C-B",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/Romanticが止まらない_C-C-B.mp3"
+  },
+  {
+    no: 15,
+    title: "あー夏休み",
+    artist: "TUBE",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/あー夏休み_TUBE.mp3"
+  },
+  {
+    no: 16,
+    title: "アジアの純真",
+    artist: "PUFFY",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/アジアの純真_PUFFY.mp3"
+  },
+  {
+    no: 17,
+    title: "あずさ2号",
+    artist: "狩人",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/あずさ2号_狩人.mp3"
+  },
+  {
+    no: 18,
+    title: "カサブランカ・ダンディ",
+    artist: "沢田研二",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/カサブランカ・ダンディ_沢田研二.mp3"
+  },
+  {
+    no: 19,
+    title: "ガッツだぜ!!",
+    artist: "ウルフルズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ガッツだぜ!!_ウルフルズ.mp3"
+  },
+  {
+    no: 20,
+    title: "カナダからの手紙",
+    artist: "平尾昌晃&畑中葉子",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/カナダからの手紙_平尾昌晃&畑中葉子.mp3"
+  },
+  {
+    no: 21,
+    title: "かもめが翔んだ日",
+    artist: "渡辺真知子",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/かもめが翔んだ日_渡辺真知子.mp3"
+  },
+  {
+    no: 22,
+    title: "ガラスのメモリーズ",
+    artist: "TUBE",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ガラスのメモリーズ_TUBE.mp3"
+  },
+  {
+    no: 23,
+    title: "カリフォルニア・コネクション",
+    artist: "水谷豊",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/カリフォルニア・コネクション_水谷豊.mp3"
+  },
+  {
+    no: 24,
+    title: "ガンダーラ",
+    artist: "Godiego",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ガンダーラ_Godiego.mp3"
+  },
+  {
+    no: 25,
+    title: "クリスマス・イブ",
+    artist: "山下達郎",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/クリスマス・イブ_山下達郎.mp3"
+  },
+  {
+    no: 26,
+    title: "クリスマスキャロルの頃には",
+    artist: "稲垣潤一",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/クリスマスキャロルの頃には_稲垣潤一.mp3"
+  },
+  {
+    no: 27,
+    title: "ゲレンデがとけるほど恋したい",
+    artist: "広瀬香美",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ゲレンデがとけるほど恋したい_広瀬香美.mp3"
+  },
+  {
+    no: 28,
+    title: "サウスポー",
+    artist: "ピンクレディ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/サウスポー_ピンクレディ.mp3"
+  },
+  {
+    no: 29,
+    title: "シーソーゲーム ～勇敢な恋の歌～",
+    artist: "Mr Children",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/シーソーゲーム%20-勇敢な恋の歌-_Mr%20Children.mp3"
+  },
+  {
+    no: 30,
+    title: "シクラメンのかほり",
+    artist: "布施明",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/シクラメンのかほり_布施明.mp3"
+  },
+  {
+    no: 31,
+    title: "ダンシング・オールナイト",
+    artist: "もんた&ブラザーズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ダンシング・オールナイト_もんた&ブラザーズ.mp3"
+  },
+  {
+    no: 32,
+    title: "ダンシング・ヒーロー",
+    artist: "荻野目洋子",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ダンシング・ヒーロー_荻野目洋子.mp3"
+  },
+  {
+    no: 33,
+    title: "チャコの海岸物語",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/チャコの海岸物語_サザンオールスターズ.mp3"
+  },
+  {
+    no: 34,
+    title: "とんぼ",
+    artist: "長渕剛",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/とんぼ_長渕剛.mp3"
+  },
+  {
+    no: 35,
+    title: "ハナミズキ",
+    artist: "一青窈",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ハナミズキ_一青窈.mp3"
+  },
+  {
+    no: 36,
+    title: "ひとり上手",
+    artist: "中島みゆき",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ひとり上手_中島みゆき.mp3"
+  },
+  {
+    no: 37,
+    title: "ファンキー・モンキー・ベイビー",
+    artist: "CAROL",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ファンキー・モンキー・ベイビー_CAROL.mp3"
+  },
+  {
+    no: 38,
+    title: "フライディ・チャイナタウン",
+    artist: "泰葉",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/フライディ・チャイナタウン_泰葉.mp3"
+  },
+  {
+    no: 39,
+    title: "プレイバック part2",
+    artist: "山口百恵",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/プレイバック%20part2_山口百恵.mp3"
+  },
+  {
+    no: 40,
+    title: "ペガサスの朝",
+    artist: "五十嵐浩晃",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ペガサスの朝_五十嵐浩晃.mp3"
+  },
+  {
+    no: 41,
+    title: "メモリーグラス",
+    artist: "堀江淳",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/メモリーグラス_堀江淳.mp3"
+  },
+  {
+    no: 42,
+    title: "もしもピアノが弾けたなら",
+    artist: "西田敏行",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/もしもピアノが弾けたなら_西田敏行.mp3"
+  },
+  {
+    no: 43,
+    title: "ら・ら・ら",
+    artist: "大黒摩季",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ら・ら・ら_大黒摩季.mp3"
+  },
+  {
+    no: 44,
+    title: "ラブ・ストーリーは突然に",
+    artist: "小田和正",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ラブ・ストーリーは突然に_小田和正.mp3"
+  },
+  {
+    no: 45,
+    title: "ランナウェイ",
+    artist: "ラッツ&スター",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ランナウェイ_ラッツ&スター.mp3"
+  },
+  {
+    no: 46,
+    title: "リンダ リンダ",
+    artist: "THE BLUE HEARTS",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/リンダ%20リンダ_THE%20BLUE%20HEARTS.mp3"
+  },
+  {
+    no: 47,
+    title: "ルージュの伝言",
+    artist: "荒井由実",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ルージュの伝言_荒井由実.mp3"
+  },
+  {
+    no: 48,
+    title: "ろくなもんじゃねぇ",
+    artist: "長渕剛",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ろくなもんじゃねぇ_長渕剛.mp3"
+  },
+  {
+    no: 49,
+    title: "ロビンソン",
+    artist: "スピッツ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ロビンソン_スピッツ.mp3"
+  },
+  {
+    no: 50,
+    title: "ロマンス",
+    artist: "岩崎宏美",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ロマンス_岩崎宏美.mp3"
+  },
+  {
+    no: 51,
+    title: "ロマンスの神様",
+    artist: "広瀬香美",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ロマンスの神様_広瀬香美.mp3"
+  },
+  {
+    no: 52,
+    title: "ロンリー・チャップリン",
+    artist: "鈴木聖美",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ロンリー・チャップリン_鈴木聖美.mp3"
+  },
+  {
+    no: 53,
+    title: "ワインレッドの心",
+    artist: "井上陽水",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/ワインレッドの心_井上陽水.mp3"
+  },
+  {
+    no: 54,
+    title: "愛のメモリー",
+    artist: "松崎しげる",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/愛のメモリー_松崎しげる.mp3"
+  },
+  {
+    no: 55,
+    title: "異邦人",
+    artist: "久保田早紀",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/異邦人_久保田早紀.mp3"
+  },
+  {
+    no: 56,
+    title: "雨の慕情",
+    artist: "八代亜紀",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/雨の慕情_八代亜紀.mp3"
+  },
+  {
+    no: 57,
+    title: "雨音はショパンの調べ",
+    artist: "小林麻美",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/雨音はショパンの調べ_小林麻美.mp3"
+  },
+  {
+    no: 58,
+    title: "壊れかけのRadio",
+    artist: "徳永英明",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/壊れかけのRadio_徳永英明.mp3"
+  },
+  {
+    no: 59,
+    title: "季節の中で",
+    artist: "松山千春",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/季節の中で_松山千春.mp3"
+  },
+  {
+    no: 60,
+    title: "君のひとみは10000ボルト",
+    artist: "アリス",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/君のひとみは10000ボルト_アリス.mp3"
+  },
+  {
+    no: 61,
+    title: "君は1000%",
+    artist: "オメガトライブ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/君は1000%_オメガトライブ.mp3"
+  },
+  {
+    no: 62,
+    title: "結婚しようよ",
+    artist: "吉田拓郎",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/結婚しようよ_吉田拓郎.mp3"
+  },
+  {
+    no: 63,
+    title: "港のヨーコ・ヨコハマ・ヨコスカ",
+    artist: "ダウン タウン ブギウギ バンド",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/港のヨーコ・ヨコハマ・ヨコスカ_ダウン%20タウン%20ブギウ%20バンド.mp3"
+  },
+  {
+    no: 64,
+    title: "止まらないHa～Ha",
+    artist: "矢沢永吉",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/止まらないHa-Ha_矢沢永吉.mp3"
+  },
+  {
+    no: 65,
+    title: "時間よ止まれ",
+    artist: "矢沢永吉",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/時間よ止まれ_矢沢永吉.mp3"
+  },
+  {
+    no: 66,
+    title: "時代",
+    artist: "中島みゆき",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/時代_中島みゆき.mp3"
+  },
+  {
+    no: 67,
+    title: "舟唄",
+    artist: "八代亜紀",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/舟唄_八代亜紀.mp3"
+  },
+  {
+    no: 68,
+    title: "初恋",
+    artist: "村下孝蔵",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/初恋_村下孝蔵.mp3"
+  },
+  {
+    no: 69,
+    title: "暑中お見舞い申し上げます",
+    artist: "キャンディーズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/暑中お見舞い申し上げます_キャンディーズ.mp3"
+  },
+  {
+    no: 70,
+    title: "勝手にしやがれ",
+    artist: "沢田研二",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/勝手にしやがれ_沢田研二.mp3"
+  },
+  {
+    no: 71,
+    title: "小さな恋のうた",
+    artist: "MONGOL800",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/小さな恋のうた_MONGOL800.mp3"
+  },
+  {
+    no: 72,
+    title: "飾りじゃないのよ 涙は",
+    artist: "中森明菜",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/飾りじゃないのよ%20涙は_井上陽水.mp3"
+  },
+  {
+    no: 73,
+    title: "神田川",
+    artist: "かぐや姫",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/神田川_かぐや姫.mp3"
+  },
+  {
+    no: 74,
+    title: "聖母(マドンナ)たちのララバイ",
+    artist: "岩崎宏美",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/聖母(マドンナ)たちのララバイ_岩崎宏美.mp3"
+  },
+  {
+    no: 75,
+    title: "贈る言葉",
+    artist: "海援隊",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/贈る言葉_海援隊.mp3"
+  },
+  {
+    no: 76,
+    title: "大都会",
+    artist: "クリスタルキング",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/大都会_クリスタルキング.mp3"
+  },
+  {
+    no: 77,
+    title: "長い夜",
+    artist: "松山千春",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/長い夜_松山千春.mp3"
+  },
+  {
+    no: 78,
+    title: "天城越え",
+    artist: "石川さゆり",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/天城越え_石川さゆり.mp3"
+  },
+  {
+    no: 79,
+    title: "冬の稲妻",
+    artist: "アリス",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/冬の稲妻_アリス.mp3"
+  },
+  {
+    no: 80,
+    title: "燃えろいい女",
+    artist: "ツイスト",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/燃えろいい女_ツイスト.mp3"
+  },
+  {
+    no: 81,
+    title: "飛んでイスタンブール",
+    artist: "庄野真代",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/飛んでイスタンブール_庄野真代.mp3"
+  },
+  {
+    no: 82,
+    title: "万里の河",
+    artist: "チャゲ&飛鳥",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/万里の河_チャゲ&飛鳥.mp3"
+  },
+  {
+    no: 83,
+    title: "魅せられて",
+    artist: "ジュディ オング",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/魅せられて_ジュディ%20オング.mp3"
+  },
+  {
+    no: 84,
+    title: "無縁坂",
+    artist: "グレープ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/無縁坂_グレープ.mp3"
+  },
+  {
+    no: 85,
+    title: "名もなき詩",
+    artist: "Mr Children",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/名もなき詩_Mr%20Children.mp3"
+  },
+  {
+    no: 86,
+    title: "翼の折れたエンジェル",
+    artist: "中村あゆみ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/翼の折れたエンジェル_中村あゆみ.mp3"
+  },
+  {
+    no: 87,
+    title: "恋におちて",
+    artist: "小林明子",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/恋におちて_小林明子.mp3"
+  },
+  {
+    no: 88,
+    title: "恋人よ",
+    artist: "五輪真弓",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/恋人よ_五輪真弓.mp3"
+  },
+  {
+    no: 89,
+    title: "路地裏の少年",
+    artist: "浜田省吾",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/路地裏の少年_浜田省吾.mp3"
+  },
+  {
+    no: 90,
+    title: "浪漫飛行",
+    artist: "米米クラブ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/BGM/浪漫飛行_米米クラブ.mp3"
+  }
+];
