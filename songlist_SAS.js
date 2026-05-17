abaffc952dfae9342a6a01a8ae34357104017a5b
qianyey204-lab <qianyey204@gmail.com>
2026-05-17 16:56:32 +0900

    Add files via upload

---
+++ songlist_SAS.js
@@ -0,0 +1,111 @@
+// songlist_sas.js
+export const songListSAS = [
+  {
+    no: 1,
+    title: "Bye Bye My Love",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/Bye%20Bye%20My%20Love_サザンオールスターズ.mp3"
+  },
+  {
+    no: 2,
+    title: "Ｃ調言葉に御用心",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/Ｃ調言葉に御用心_サザンオールスターズ.mp3"
+  },
+  {
+    no: 3,
+    title: "Oh! クラウディア",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/Oh!%20クラウディア_サザンオールスターズ.mp3"
+  },
+  {
+    no: 4,
+    title: "いとしのエリー",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/いとしのエリー_サザンオールスターズ.mp3"
+  },
+  {
+    no: 5,
+    title: "いなせなロコモーション",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/いなせなロコモーション_サザンオールスターズ.mp3"
+  },
+  {
+    no: 6,
+    title: "チャコの海岸物語",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/チャコの海岸物語_サザンオールスターズ.mp3"
+  },
+  {
+    no: 7,
+    title: "ミス・ブランニュー・デイ",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/ミス・ブランニュー・デイ_サザンオールスターズ.mp3"
+  },
+  {
+    no: 8,
+    title: "メロディ",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/メロディ_サザンオールスターズ.mp3"
+  },
+  {
+    no: 9,
+    title: "愛の言霊",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/愛の言霊_サザンオールスターズ.mp3"
+  },
+  {
+    no: 10,
+    title: "夏をあきらめて",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/夏をあきらめて_サザンオールスターズ.mp3"
+  },
+  {
+    no: 11,
+    title: "希望の轍",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/希望の轍_サザンオールスターズ.mp3"
+  },
+  {
+    no: 12,
+    title: "思い過ごしも恋のうち",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/思い過ごしも恋のうち_サザンオールスターズ.mp3"
+  },
+  {
+    no: 13,
+    title: "女呼んでブギ",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/女呼んでブギ_サザンオールスターズ.mp3"
+  },
+  {
+    no: 14,
+    title: "勝手にシンドバッド",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/勝手にシンドバッド_サザンオールスターズ.mp3"
+  },
+  {
+    no: 15,
+    title: "真夜中のダンディー",
+    artist: "桑田佳祐",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/真夜中のダンディー_サザンオールスターズ.mp3"
+  },
+  {
+    no: 16,
+    title: "匂艶 THE NIGHT CLUB",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/匂艶%20THE%20NIGHT%20CLUB_サザンオールスターズ.mp3"
+  },
+  {
+    no: 17,
+    title: "涙のキッス",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/涙のキッス_サザンオールスターズ.mp3"
+  },
+  {
+    no: 18,
+    title: "栞のテーマ",
+    artist: "サザンオールスターズ",
+    url: "https://raw.githubusercontent.com/qianyey204-lab/audio/main/SAS/栞のテーマ_サザンオールスターズ.mp3"
+  }
+];
